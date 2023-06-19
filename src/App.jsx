fbdcd7e0f607bb12a2f84282a2bc39d30c9107b0
jhean carlos <jheanrabelo01@gmail.com>
2023-06-19 10:49:55 -0400

    inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// react, componentes, státicos
+
+
+
+import Welcome from './components/Welcome'
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Quiz de programção </h1>
+        <Welcome />
+        </div>
+     
+    </>
+  )
+}
+
+export default App
